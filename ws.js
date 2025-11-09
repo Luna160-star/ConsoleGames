40989671c50c8a66da30dc40e0f358b56308e840
Luna160-star <rakhigoalsachieve@gmail.com>
2025-11-09 19:19:12 +0530

    Done with js and debugging, I need to debug a little more in javascript

--- ws.js
+++ ws.js
@@ -1,40 +1,65 @@
-let pressedButton = false;
+let isPressButton = false;
 let selectedBoxes = [];
 
-const boxes = document.querySelectorAll('.gridbox td')
+const boxes = document.querySelectorAll('.gridbox td');
 
 /*for looping*/
 boxes.forEach(box =>{
 
     /*this action for when we are clicking on the boxes and zctually want it to be get selected*/
     box.addEventListener('mousedown', function(e) {
-        ispressedButton = true;
+        isPressButton = true;
         clearSelection();
         startSelection(box);
-    })
+    });
 
     /*this action is for dragging, like when the user selects and drags it*/
-    box.addEventListener('mouseenter', function(e){
-        if (pressedButton) {
+    box.addEventListener('mouseenter', function(e) {
+        if (isPressButton) {
             startSelection(box);
         }
-    })
+    });
 
     /*this action is mainly for when the user leaves the selected box*/
-    box.addEventListener('mouselift', function(e){
-        ispressedButton = false;
+    box.addEventListener('mouseup', function(e) {
+        isPressButton = false;
         checkSelection();
-    })
-})
+    });
+});
 
 function startSelection(box) {
-    if (!selectedBoxes.includes((box)){
+    if (!selectedBoxes.includes(box)) {
         box.classList.add('select');
         selectedBoxes.push(box);
-    })
+    }
 
 }
 
- let word = selectedBoxes.map(box => box.textContent).join('');
+function clearSelection() {
+    selectedBoxes.forEach(box => box.classList.removal('select'));
+    selectedBoxes.forEach(box => box.classList.remove('highlight'));
+    selectedBoxes = [];
+}
 
- let answer = ['WATER', 'BAG', 'COMPASS', 'ROPE', 'FLASHLIGHT'] /*maybe i myself forgot something here, i will check it at the end*/
+function checkSelection() {
+
+let word = selectedBoxes.map(box => box.textContent).join('');
+let answerList = ['WATER', 'BAG', 'COMPASS', 'ROPE', 'FLASHLIGHT'] /*maybe i myself forgot something here, i will check it at the end*/
+
+if (answerList.includes(word)) {
+
+    selectedBoxes.forEach(cell => {
+    box.classList.remove('select');
+    box.classList.add('highlight');
+});
+
+} else {
+setTimeout(() => {
+    clearSelection();
+}, 100);
+}
+
+selectedBoxes = [];
+}
+
+document.body.onmousedown = () => false;
